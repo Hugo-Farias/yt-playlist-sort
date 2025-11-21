65068151132d8795269b5476c9eb176655e8f2d1
Hugo-Farias <hg80rod@gmail.com>
2025-11-21 07:35:52 -0300

    add reusable function for onClick

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,46 +1,62 @@
-type settingsT = {
+import type { ReactNode } from "react";
+
+type SettingsT = {
   date: boolean;
+  scroll: boolean;
 };
 
-const initialSettings: settingsT = {
-  date: false,
+const initialSettings: SettingsT = {
+  date: true,
+  scroll: false,
 };
 
+// TODO: finish this
 function App() {
-  const [settings, setSettings] = useState<settingsT>(initialSettings);
+  const [settings, setSettings] = useState<SettingsT>(initialSettings);
 
   // const onClick = () => {
   //   console.log("key down");
   //   setSettings((prev) => ({ ...prev, date: !prev.date }));
   // };
 
-  const onChange = () => {
-    setSettings((prev) => ({ ...prev, date: !prev.date }));
+  const onChange = (id: keyof SettingsT) => {
+    setSettings((prev) => {
+      console.log(prev);
+      return { ...prev, [id]: !prev[id] };
+    });
+  };
+
+  const optionEl = (id: keyof SettingsT, label: string): ReactNode => {
+    return (
+      <label
+        key={id}
+        className={
+          "inline-block w-full cursor-pointer items-center px-3 transition-colors hover:text-stone-100"
+        }
+        htmlFor={id}
+      >
+        <input
+          className={"mr-3 cursor-pointer"}
+          type={"checkbox"}
+          aria-label={label}
+          checked={settings[id]}
+          id={id}
+          onChange={() => onChange(id)}
+        />
+        {label}
+      </label>
+    );
   };
 
   return (
     <div
       className={
-        "min-w-80 select-none bg-stone-900 p-1 pb-5 text-base text-stone-300"
+        "min-w-80 select-none bg-stone-900 p-1 text-base text-stone-300"
       }
     >
-      <form className={"flex px-1"}>
-        <label
-          className={
-            "w-full cursor-pointer bg-red-700/30 px-3 transition-colors hover:bg-stone-300/15 hover:text-stone-100"
-          }
-          htmlFor={"test"}
-        >
-          <input
-            className={"mr-3 cursor-pointer"}
-            type={"checkbox"}
-            aria-label={"Toggle display dates"}
-            checked={settings.date}
-            id={"test"}
-            onChange={onChange}
-          />
-          Display Dates
-        </label>
+      <form className={"items-center space-y-1"}>
+        {optionEl("date", "Display dates")}
+        {optionEl("scroll", "Scroll to currently playing")}
       </form>
     </div>
   );
